dc5f3d85eddc0f7cee48022542164fc727d7aa01
kdjun97 <kdjun97@gmail.com>
2022-06-12 12:13:14 +0900

    Feat: Add circle button Root page에 다음페이지로 넘어가는 버튼 추가

---
+++ lib/widgets/button/root_next_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:teampool/theme/color.dart';
+
+class RootNextButton extends StatelessWidget {
+  double paddingSize = 50.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: paddingSize),
+      child: MaterialButton(
+        onPressed: () {
+          print("버튼");
+        },
+        color: const Color(rootNextButtonColor),
+        textColor: const Color(arrowColor),
+        child: const Icon(
+          Icons.arrow_forward,
+          size: 100,
+        ),
+        padding: const EdgeInsets.all(16),
+        shape: const CircleBorder(),
+      ),
+    );
+  }
+}
