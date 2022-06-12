fd9008ce9be65ccbe17a24dabbe063630e899bcf
kdjun97 <kdjun97@gmail.com>
2022-06-12 12:13:47 +0900

    Feat: Add root description text 루트 페이지에 description text 추가

---
+++ lib/widgets/text/root/root_description.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RootDescription extends StatelessWidget {
+  double fontSize = 20.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+        child: Text(
+          "저희 페이지는 마음에 맞는 팀원을 구할 수 있는 기회를 제공합니다.\n간단한 프로필을 등록하고 원하는 팀원들을 구해 팀을 꾸려보세요.",
+          style: TextStyle(
+              fontSize: fontSize,
+              fontFamily: 'NotoSansKR',
+          ),
+          textAlign: TextAlign.center,
+        )
+    );
+  }
+}
