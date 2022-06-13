7b559c3e34e62660becb539f565699b85ae9f0c9
kdjun97 <kdjun97@gmail.com>
2022-06-13 23:27:45 +0900

    Feat: Add ScreenTypeLayout widget 웹, 모바일, 태블릿 대응용 위젯 추가

--- lib/pages/root.dart
+++ lib/pages/root.dart
@@ -1,35 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:teampool/utils/custom_appbar/custom_appbar.dart';
-import 'package:teampool/widgets/button/root_next_button.dart';
-import 'package:teampool/widgets/text/root/root_description.dart';
-import 'package:teampool/widgets/text/root/root_title.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:teampool/pages/tablet/tablet_root_body.dart';
+
+import 'desktop/desktop_root_body.dart';
+import 'mobile/mobile_root_body.dart';
 
 class Root extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          CustomAppBar(
-            showInfo: true,
-            width: Get.width,
-            height: Get.height * 0.5 / 10,
-          ),
-          RootTitle(
-            height: Get.height * 1 / 10,
-          ),
-          Container(
-            width: 474,
-            height: 308,
-            child: Image.asset(
-              'assets/images/root_image.jpg',
-              fit: BoxFit.fill,
-            ),
-          ),
-          RootDescription(),
-          RootNextButton(),
-        ],
+      body: ScreenTypeLayout(
+        desktop: DesktopRootBody(),
+        tablet: TabletRootBody(),
+        mobile: MobileRootBody(),
       ),
     );
   }
