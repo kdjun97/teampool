72df5640d3fcce39c51650b2e08f09ece460bb9d
kdjun97 <kdjun97@gmail.com>
2022-06-13 23:23:59 +0900

    Feat: Add ScreenBreakpoints desktop, tablet, watch에 대응하는 break point 설정

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:teampool/root/pages.dart';
 import 'package:teampool/root/route_name.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 void main() {
+  setPathUrlStrategy();
+  ResponsiveSizingConfig.instance.setCustomBreakpoints(
+    const ScreenBreakpoints(desktop: 800, tablet: 550, watch: 200),
+  );
   runApp(const MyApp());
 }
 
