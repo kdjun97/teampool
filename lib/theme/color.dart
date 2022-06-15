feb7fee3afba0ba74e2142e474e0d04cdd43eae9
kdjun97 <kdjun97@gmail.com>
2022-06-15 21:47:01 +0900

    Feat: Add cusotom appbar background color

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -1,5 +1,6 @@
 class CustomColorTheme {
   static const appBarTextColor = 0xFF000000;
+  static const appBarBackgroundColor = 0xFFE8E8E8;
   static const rootNextButtonColor = 0xFFEFEEEE;
   static const arrowColor = 0xFF676767;
 }
