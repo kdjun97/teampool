4540c3ed24ff70e02af492f3d9726d9fdca120af
kdjun97 <kdjun97@gmail.com>
2022-06-15 21:47:53 +0900

    Feat: Add ResponsiveBuilder 다양한 사이즈에 대응하기 위해 ResponsiveBuilder Widget 추가

--- lib/pages/root.dart
+++ lib/pages/root.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:teampool/pages/tablet/tablet_root_body.dart';
+import 'package:get/get.dart';
 
 import 'desktop/desktop_root_body.dart';
 import 'mobile/mobile_root_body.dart';
@@ -9,10 +10,15 @@ class Root extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ScreenTypeLayout(
-        desktop: DesktopRootBody(),
-        tablet: TabletRootBody(),
-        mobile: MobileRootBody(),
+      body: ResponsiveBuilder(
+        builder: (context, sizingInformation) {
+          print("${Get.width}");
+          return ScreenTypeLayout(
+            desktop: DesktopRootBody(),
+            tablet: TabletRootBody(),
+            mobile: MobileRootBody(),
+          );
+        },
       ),
     );
   }
