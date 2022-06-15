2da7b773929681ac00d0aafbb03a7f4eadf68a23
kdjun97 <kdjun97@gmail.com>
2022-06-15 21:40:14 +0900

    Feat: Add ReponsiveBuilder Widget 창 Size가 달라지는 것을 반응하게 도와주는 widget 추가

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'teampool',
       initialRoute: RouteName.ROOT,
       getPages: Pages.routes,
