3a5336f1566af0037f9ee4d03c0f88be2edd913f
kdjun97 <kdjun97@gmail.com>
2022-06-30 22:59:42 +0900

    Feat: Add color theme Add form background color

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -3,4 +3,5 @@ class CustomColorTheme {
   static const appBarBackgroundColor = 0xFFE8E8E8;
   static const rootNextButtonColor = 0xFFEFEEEE;
   static const arrowColor = 0xFF676767;
+  static const formBackgroundColor = 0xFFF6F6F6;
 }
